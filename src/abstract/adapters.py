e0a80a343f9ddbcbc7502af454b767095e732962
Andrew Sergienko <sergienkoandrew9922@gmail.com>
2023-08-04 16:26:06 +0300

    feat: create WebClient, FileManager adapters

---
+++ src/abstract/adapters.py
@@ -0,0 +1,19 @@
+from abc import ABC, abstractmethod
+
+from src.domain import File
+
+
+class AWebClient(ABC):
+    @abstractmethod
+    async def download_file(self, link: str):
+        pass
+
+
+class AFileManager(ABC):
+    @abstractmethod
+    async def save_file(self, files_dir, file: File, name: str):
+        pass
+
+    @abstractmethod
+    async def delete_file(self, files_dir, file_name: str):
+        pass
---
+++ src/adapters.py
@@ -0,0 +1,54 @@
+import os
+import string
+from datetime import datetime
+from pathlib import Path
+from random import choices
+
+import aiofiles
+from aiohttp import ClientSession
+
+from src.abstract import adapters as abstract
+from src.domain import File
+
+
+class WebClient(abstract.AWebClient):
+    async def download_file(self, link: str) -> File:
+        async with ClientSession() as session:
+            async with session.get(link) as resp:
+                file_type = resp.content_type.split("/")[1]
+                return File(file_type, await resp.read())
+
+
+class FileManager(abstract.AFileManager):
+    async def save_file(
+        self, files_dir: Path, file: File, name: str | None = None
+    ) -> str:
+        """
+        Saving the file in the system.
+        :param files_dir: dir of files
+        :param file: bytes
+        :param name: file name. If None, then generate
+        :return: file name
+        """
+        if name and os.path.exists(files_dir / name):
+            # If a file with the given name already exists,
+            # it means it has already been saved.
+            return name
+
+        filename = name if name else self.generate_unique_filename()
+        while os.path.exists(files_dir / filename):
+            # If a file with that name already exists, generate a new one
+            filename = self.generate_unique_filename()
+
+        async with aiofiles.open(files_dir / f"{filename}.{file.file_type}", "wb") as f:
+            await f.write(file.content)
+            return f"{filename}.{file.file_type}"
+
+    async def delete_file(self, files_dir: Path, file_name: str):
+        pass
+
+    def generate_unique_filename(self) -> str:
+        """Getting a unique file name in the format <random_part>_<timestamp>."""
+        basename = "".join(choices(string.ascii_letters + string.digits, k=5))
+        suffix = int(datetime.now().timestamp())
+        return f"{basename}_{suffix}"
