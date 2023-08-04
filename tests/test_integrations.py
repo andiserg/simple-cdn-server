f363b45f1f45651c77feea67afa44d0a45c73e1a
Andrew Sergienko <sergienkoandrew9922@gmail.com>
2023-08-04 23:02:44 +0300

    refactor: refactor tests

---
+++ tests/test_integrations.py
@@ -0,0 +1,20 @@
+import pytest
+
+
+@pytest.mark.asyncio
+async def test_upload_file(cli, loop):
+    file_link = "https://freetestdata.com/wp-content/uploads/2023/04/1.17-MB.bmp"
+    response = await cli.post("/files/", data={"link": file_link})
+    response_json = await response.json()
+
+    required_fields = [
+        "name",
+        "city",
+        "ip",
+        "download_duration",
+        "time",
+        "file_link",
+        "origin_link",
+    ]
+    assert response.status == 200
+    assert all([field in response_json for field in required_fields])
