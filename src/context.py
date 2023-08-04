673d0e6b0f243fa4f2ba94e6a2b10f9547056c3c
Andrew Sergienko <sergienkoandrew9922@gmail.com>
2023-08-04 16:28:24 +0300

    feat: add fields in Context class

--- src/context.py
+++ src/context.py
@@ -1,2 +1,15 @@
+from pathlib import Path
+
+from src import adapters
+from src.abstract import adapters as abstract
+
+
 class Context:
-    pass
+    web: abstract.AWebClient
+    files: abstract.AFileManager
+    FILES_DIR: Path
+
+    def __init__(self):
+        self.web = adapters.WebClient()
+        self.files = adapters.FileManager()
+        self.FILES_DIR = Path(__file__).parent.parent / "files"
