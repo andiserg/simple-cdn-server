2659c122a6999b63079894c31034ef12b190861a
Andrew Sergienko <sergienkoandrew9922@gmail.com>
2023-08-04 23:01:49 +0300

    feat: add Timer for measuring code execution time

--- src/routes.py
+++ src/routes.py
@@ -1,9 +1,9 @@
-import os
+from datetime import datetime
 
 from aiohttp import web
 from aiohttp.web_routedef import RouteDef
 
-from src import services
+from src import services, utils
 from src.context import Context
 
 
@@ -19,16 +19,31 @@ class Handlers:
     def __init__(self, context: Context):
         self.context = context
 
-    async def get_zone(self, request: web.Request):
-        return web.Response(text=os.environ.get("ZONE"))
-
     async def download_file_handler(self, request: web.Request):
+        # checking the presence of required fields
         data = await request.post()
         link = data.get("link")
         if not link:
             raise web.HTTPBadRequest(reason="link is required.")
 
-        file = await services.download_file(self.context, link)
-        file.name = file.name if not data.get("name") else data.get("name")
-        file_name = await services.save_file(self.context, file)
-        return web.json_response({"link": file_name}, status=200)
+        timer = utils.Timer()
+        # starting the timer for measuring the download time of the file
+        async with timer:
+            # downloading the content of the file
+            file = await services.download_file(self.context, link)
+            # 'name' is passed in the case of file replication
+            file.name = file.name if not data.get("name") else data.get("name")
+            # saving the file in the file system
+            file_name = await services.save_file(self.context, file)
+
+        # generating the response
+        response = {
+            "name": await self.context.env.get("NAME"),
+            "city": await self.context.env.get("CITY"),
+            "ip": await self.context.env.get("IP"),
+            "download_duration": timer.execution_time,
+            "time": datetime.now().strftime("%Y-%m-%d %H:%M:%S"),
+            "file_link": file_name,
+            "origin_link": link,
+        }
+        return web.json_response(response, status=200)
