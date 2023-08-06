6df5529816a21e2f13deb365618c6a73468ddc12
Andrew Srg <sergienkoandrew9922@gmail.com>
2023-08-06 20:50:13 +0300

    refactor: create send file status service

--- src/services/handlers.py
+++ src/services/handlers.py
@@ -1,1 +1,22 @@
+from src.abstract.context import AContext
+from src.domain.events import FileReplicatedEvent
+from src.domain.model import ReplicatedFileStatus
+
 EVENT_HANDLERS = {}
+
+
+async def send_replicated_file_status(context: AContext, event: FileReplicatedEvent):
+    """
+    Send a message to the origin server about the successful replication of the file.
+    :param context: Context instance
+    :param event: file replicated event
+    """
+    files_url = await context.env.get("FILES_URL")
+    status = ReplicatedFileStatus(
+        file_url=f"{files_url}/files/{event.file.name}.{event.file.file_type}",
+        origin_url=event.file.origin_url,
+        server=event.server,
+        duration=event.duration,
+        time=event.time,
+    )
+    await context.web.send_file_status(await context.env.get("ORIGIN_URL"), status)
