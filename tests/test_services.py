6755e62b0911c24d8746b9d0989c41653b889b34
Andrew Srg <sergienkoandrew9922@gmail.com>
2023-08-06 20:30:39 +0300

    refactor: refactor services tests

--- tests/test_services.py
+++ tests/test_services.py
@@ -7,7 +7,7 @@ from src.services.commands import replication_file
 
 @pytest.mark.asyncio
 async def test_replication_file_with_file_should_publish_event(fake_context):
-    file = File(content=b"Hello world", file_type="txt")
+    file = File(content=b"Hello world", file_type="txt", origin_url="test")
     await replication_file(fake_context, file)
 
     assert isinstance(fake_context.events.events[-1], FileReplicatedEvent)
