1e795da2353af9eaa6aad8044e46e8b1c6ef53bf
Andrew Srg <sergienkoandrew9922@gmail.com>
2023-08-06 16:53:13 +0300

    feat: create Event Manager class

---
+++ src/services/event_manager.py
@@ -0,0 +1,30 @@
+import asyncio
+from collections import defaultdict
+from typing import Callable
+
+from src.context import Context
+from src.domain.events import Event
+
+
+class EventManager:
+    def __int__(self):
+        self.subscribers = defaultdict(list)
+
+    async def subscribe(self, event_class: Event, callback: Callable):
+        """
+        Add a method as a handler for an event.
+        :param event_class: Event class
+        :param callback: callable method
+        """
+        self.subscribers[event_class].append(callback)
+
+    async def publish(self, context: Context, event: Event):
+        """
+        Run event handlers in the background in an asynchronous mode
+        :param context: Context. Needed for method execution
+        :param event: object of a child class of the Event class
+        """
+        if type(event) in self.subscribers:
+            for callback in self.subscribers[type(event)]:
+                # launching a background asynchronous task for the event handler.
+                asyncio.create_task(callback(context, event))
