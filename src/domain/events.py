a62340ba41d3d66783037ab20e9e166ae33cdcc4
Andrew Srg <sergienkoandrew9922@gmail.com>
2023-08-06 16:53:00 +0300

    feat: Add Event class as parent

--- src/domain/events.py
+++ src/domain/events.py
@@ -3,12 +3,16 @@ from dataclasses import dataclass
 from src.domain.model import File, Server
 
 
+class Event:
+    pass
+
+
 @dataclass
-class FileDownloadedEvent:
+class FileDownloadedEvent(Event):
     file: File
 
 
 @dataclass
-class FileReplicatedEvent:
+class FileReplicatedEvent(Event):
     file: File
     server: Server
