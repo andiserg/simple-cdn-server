13675647e1a489c251a01f156abb5febfe8061ae
Andrew Srg <sergienkoandrew9922@gmail.com>
2023-08-07 10:45:00 +0300

    feat: add replicated file test

--- tests/test_services.py
+++ tests/test_services.py
@@ -1,12 +1,13 @@
 import asyncio
+from datetime import datetime
 
 import pytest
 
 from src.abstract.context import AContext
 from src.domain.events import Event, FileReplicatedEvent
-from src.domain.model import File
-from src.services.commands import replication_file
+from src.domain.model import File, Server
 from src.services.event_manager import EventManager
+from src.services.handlers import replication_file, send_replicated_file_status
 
 
 @pytest.mark.asyncio
@@ -15,6 +16,22 @@ async def test_replication_file_with_file_should_publish_event(fake_context):
     await replication_file(fake_context, file)
 
     assert isinstance(fake_context.events.events[-1], FileReplicatedEvent)
+
+
+@pytest.mark.asyncio
+async def test_send_replicated_file_status_with_correct_data_return_status(
+    fake_context,
+):
+    file = File(name="test", file_type="txt", content=b"Hello world", origin_url="test")
+    server = Server(name="TestVPS", ip="0.0.0.0", zone="test")
+    event = FileReplicatedEvent(
+        file=file, duration=10, time=datetime.now(), server=server
+    )
+    try:
+        # method will pass the test if it executes without errors
+        await send_replicated_file_status(fake_context, event)
+    except Exception:
+        pytest.fail("raise Exception")
 
 
 @pytest.mark.asyncio
