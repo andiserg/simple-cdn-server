1fbad7eb9ed8b31e1057bde9611228052def8fa8
Andrew Srg <sergienkoandrew9922@gmail.com>
2023-08-07 18:44:43 +0300

    refactor: refactor tests

--- tests/test_adapters.py
+++ tests/test_adapters.py
@@ -8,21 +8,32 @@ from src.domain.model import FileInfo, Server
 @pytest.mark.asyncio
 async def test_web_client_download_file_with_correct_data_should_download(context):
     link = "https://freetestdata.com/wp-content/uploads/2023/04/1.17-MB.bmp"
-    file = await context.web.download_file(link)
+    file = await context.web.download_and_save_file(
+        link, context.FILES_DIR, "test", context.files.save_file
+    )
 
     assert isinstance(file, FileInfo)
-    assert isinstance(file.content, bytes)
+    assert os.path.exists(context.FILES_DIR / "test.bmp")
+
+    # delete file
+    os.remove(context.FILES_DIR / "test.bmp")
 
 
 @pytest.mark.asyncio
 async def test_file_manager_save_file_with_correct_data_should_save(context):
-    file = FileInfo(content=b"Hello world", file_type="txt", origin_url="test")
-    file_name = await context.files.save_file(context.FILES_DIR, file)
+    async def test_chunks_iterator():
+        test_data = b"Hello, World"
+        chunk_size = 5
 
-    assert os.path.exists(context.FILES_DIR / file_name)
+        for i in range(0, len(test_data), chunk_size):
+            yield (test_data[i : i + chunk_size], False)
+
+    await context.files.save_file(context.FILES_DIR, "test.txt", test_chunks_iterator())
+
+    assert os.path.exists(context.FILES_DIR / "test.txt")
 
     # delete file
-    os.remove(context.FILES_DIR / file_name)
+    os.remove(context.FILES_DIR / "test.txt")
 
 
 @pytest.mark.asyncio
--- tests/test_services.py
+++ tests/test_services.py
@@ -1,10 +1,11 @@
 import asyncio
 from datetime import datetime
+from pathlib import Path
 
 import pytest
 
 from src.abstract.context import AContext
-from src.domain.events import Event, FileReplicatedEvent
+from src.domain.events import Event, FileReplicatedEvent, FileSavedEvent
 from src.domain.model import FileInfo, Server
 from src.services.event_manager import EventManager
 from src.services.handlers import replication_file, send_replicated_file_status
@@ -12,8 +13,10 @@ from src.services.handlers import replication_file, send_replicated_file_status
 
 @pytest.mark.asyncio
 async def test_replication_file_with_file_should_publish_event(fake_context):
-    file = FileInfo(content=b"Hello world", file_type="txt", origin_url="test")
-    await replication_file(fake_context, file)
+    chunk_iterator = await fake_context.files.get_chunk_iterator(Path(), 10)
+    file_info = FileInfo(name="text", file_type="txt", origin_url="test")
+    event = FileSavedEvent(file_info, chunk_iterator)
+    await replication_file(fake_context, event)
 
     assert isinstance(fake_context.events.events[-1], FileReplicatedEvent)
 
@@ -22,9 +25,7 @@ async def test_replication_file_with_file_should_publish_event(fake_context):
 async def test_send_replicated_file_status_with_correct_data_return_status(
     fake_context,
 ):
-    file = FileInfo(
-        name="test", file_type="txt", content=b"Hello world", origin_url="test"
-    )
+    file = FileInfo(name="test", file_type="txt", origin_url="test")
     server = Server(name="TestVPS", ip="0.0.0.0", zone="test")
     event = FileReplicatedEvent(
         file=file, duration=10, time=datetime.now(), server=server
@@ -50,7 +51,7 @@ async def test_event_manager_publish_with_handlers_should_process(fake_context):
 
     event_manager = EventManager()
     # subscribe handler to TestEvent
-    await event_manager.subscribe(TestEvent, handler)
+    event_manager.subscribe(TestEvent, handler)
 
     event = TestEvent()
     await event_manager.publish(fake_context, event)
