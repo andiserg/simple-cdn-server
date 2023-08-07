a99a1c426a9c8ebbe3c90d0a0568e0c0d6312111
Andrew Srg <sergienkoandrew9922@gmail.com>
2023-08-07 23:55:58 +0300

    feat: add tests to upload file route handler

--- tests/test_integrations.py
+++ tests/test_integrations.py
@@ -1,10 +1,11 @@
 import asyncio
+import os
 
 import pytest
 
 
 @pytest.mark.asyncio
-async def test_download_file_with_correct_data_return_link(cli, test_server):
+async def test_download_file_from_link_with_correct_data_return_200(cli, test_server):
     await test_server
 
     file_link = "https://freetestdata.com/wp-content/uploads/2023/04/1.05KB_JSON-File_FreeTestData.json"
@@ -12,3 +13,18 @@ async def test_download_file_with_correct_data_return_link(cli, test_server):
     await asyncio.sleep(1)
 
     assert response.status == 200
+
+
+@pytest.mark.asyncio
+async def test_upload_file_with_correct_data_return_200(cli, test_server, context):
+    await test_server
+    data = b"Hello, world"
+    headers = {"FILE_NAME": "test.txt"}
+
+    response = await cli.put("/files/", data=data, headers=headers)
+    await asyncio.sleep(1)
+
+    assert response.status == 200
+
+    # delete file
+    os.remove(context.FILES_DIR / "test.txt")
