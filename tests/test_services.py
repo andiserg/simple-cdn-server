8b04f24c9658b03095079abca2056acd164fb5af
Andrew Srg <sergienkoandrew9922@gmail.com>
2023-08-07 09:14:11 +0300

    feat: add tests to event manager

--- tests/test_services.py
+++ tests/test_services.py
@@ -1,8 +1,12 @@
+import asyncio
+
 import pytest
 
-from src.domain.events import FileReplicatedEvent
+from src.abstract.context import AContext
+from src.domain.events import Event, FileReplicatedEvent
 from src.domain.model import File
 from src.services.commands import replication_file
+from src.services.event_manager import EventManager
 
 
 @pytest.mark.asyncio
@@ -11,3 +15,28 @@ async def test_replication_file_with_file_should_publish_event(fake_context):
     await replication_file(fake_context, file)
 
     assert isinstance(fake_context.events.events[-1], FileReplicatedEvent)
+
+
+@pytest.mark.asyncio
+async def test_event_manager_publish_with_handlers_should_process(fake_context):
+    # create event class
+    class TestEvent(Event):
+        def __init__(self):
+            # contains the names of handlers that processed the event
+            self.processed_handlers = []
+
+    # create handler
+    async def handler(context: AContext, event: TestEvent):
+        event.processed_handlers.append("handler")
+
+    event_manager = EventManager()
+    # subscribe handler to TestEvent
+    await event_manager.subscribe(TestEvent, handler)
+
+    event = TestEvent()
+    await event_manager.publish(fake_context, event)
+    # Provide control to the handler
+    # and wait until it completes its task
+    await asyncio.sleep(0.001)
+
+    assert "handler" in event.processed_handlers
