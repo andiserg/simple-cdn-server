61ca4e824ecc8ad01cc750c0eaffa9ae238cd4d0
Andrew Srg <sergienkoandrew9922@gmail.com>
2023-08-14 17:47:13 +0300

    Revert "refactor: refactor bootstrap app to gunicorn"
    This reverts commit fd5d813ad7704d0be905deee0a44d968b9666cc6.

--- src/main.py
+++ src/main.py
@@ -1,4 +1,5 @@
+from aiohttp import web
+
 from src.bootstrap import init_app
 
-app = init_app()  # for gunicorn
-# web.run_app(init_app(), host="0.0.0.0", port=8000)
+web.run_app(init_app(), host="0.0.0.0", port=8000)
